17e0224a05b3fd30e8d1c76f1f48cced079b5dba
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2007-07-09 10:02:05 +0000

    Erweiterungen

--- trunk/Jicker/org/jicker/gui/JickerApp.java
+++ trunk/Jicker/org/jicker/gui/JickerApp.java
@@ -6,12 +6,15 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import com.googlepages.dronten.jtagger.gui.MainWindow;
+
 public class JickerApp extends JFrame {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private MainWindow aMainWindow = null;
 	Container c;
 	JLabel beschriftung;
 	public JickerApp(String appName, String prefName) {
