6d19a0077fc288201dcb216012e955805d9e9c70
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2007-07-09 10:02:05 +0000

    Erweiterungen

---
+++ Jicker/org/jicker/gui/BaseWindow.java
@@ -0,0 +1,16 @@
+package org.jicker.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * The Class BaseWindow.
+ */
+public class BaseWindow extends JPanel {
+
+/**
+ * Instantiates a new base window.
+ */
+public BaseWindow(){
+	super();
+}
+}
---
+++ Jicker/org/jicker/gui/MainWindow.java
@@ -0,0 +1,22 @@
+package org.jicker.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+
+public class MainWindow extends BaseWindow{
+	public MainWindow(){
+        JSplitPane aSplitPanel = new JSplitPane();
+        LeftPanel aLeftPanel = new LeftPanel();
+        JTabbedPane aCenterPanel = new JTabbedPane();
+        StatusBar aStatusBar = new StatusBar();
+
+        setLayout(new BorderLayout());
+        aSplitPanel.setLeftComponent(aLeftPanel);
+        aSplitPanel.setRightComponent(aCenterPanel);
+        add(aSplitPanel, BorderLayout.CENTER);
+        add(aStatusBar, BorderLayout.SOUTH);
+	}
+
+}
