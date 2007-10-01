549a64bedf21fbc4c936cb141454a8d69a335501
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2007-10-01 20:06:40 +0000

    Update des GUI, Statusbar hinzugefügt

--- trunk/Jicker/org/jicker/gui/MainWindow.java
+++ trunk/Jicker/org/jicker/gui/MainWindow.java
@@ -5,7 +5,6 @@ import java.awt.BorderLayout;
 import javax.swing.JList;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
-import javax.swing.UIManager;
 
 public class MainWindow extends BaseWindow{
 	public MainWindow(){
@@ -14,16 +13,14 @@ public class MainWindow extends BaseWindow{
         JTabbedPane aCenterPanel = new JTabbedPane();
         String[] test = {"a","b","c"};
         JList list = new JList(test);
-        //StatusBar aStatusBar = new StatusBar();
-
- 
+        StatusBar aStatusBar = new StatusBar(null,null,null,"Test");
          
         setLayout(new BorderLayout());
         //aSplitPanel.setLeftComponent(aLeftPanel);
         aSplitPanel.setLeftComponent(list);
         aSplitPanel.setRightComponent(aCenterPanel);
         add(aSplitPanel, BorderLayout.CENTER);
-        //add(aStatusBar, BorderLayout.SOUTH);
+        add(aStatusBar, BorderLayout.SOUTH);
 	}
 
 }
