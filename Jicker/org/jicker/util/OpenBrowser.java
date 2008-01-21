6685bb7dc4b6285b25dbc407ac67e161f2d0a682
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2008-01-21 18:27:01 +0000

    Start des IExplorer

---
+++ Jicker/org/jicker/util/OpenBrowser.java
@@ -0,0 +1,50 @@
+package org.jicker.util;
+
+class OpenBrowser {
+	final String WIN_ID = "Win";
+
+	public OpenBrowser(java.io.File file) {
+		if (isWindowsPlatform()) {
+			try {
+				Runtime.getRuntime().exec(tryCommand1() + file);
+			} catch (Exception exc1) {
+				try {
+					Runtime.getRuntime().exec(tryCommand2() + file);				
+					} catch (Exception exc2) {
+					try {
+						Runtime.getRuntime().exec(tryCommand3() + file);			
+						} catch (Exception exc3) {
+						// if(MainEditor.getOpenBrowserHelper() ) new
+						// OpenBrowserHelper(file);
+					}
+				}
+			}
+		} else
+			javax.swing.JOptionPane
+					.showMessageDialog(
+							null,
+							"<html><center>"
+									+ "Not a Windows Operating System. Contact software author for details"
+									+ "</html></center>", "Not Using Windows",
+							javax.swing.JOptionPane.WARNING_MESSAGE);
+	}
+
+	private boolean isWindowsPlatform() {
+		String os = System.getProperty("os.name");
+		if (os != null && os.startsWith(WIN_ID))
+			return true;
+		return false;
+	}
+
+	private String tryCommand1() {
+		return "C:\\Programme\\Internet Explorer\\Iexplore.exe file://";
+	}
+
+	private String tryCommand2() {
+		return "start rundll32 url.dll,FileProtocolHandler file://";
+	}
+
+	private String tryCommand3() {
+		return "file://";
+	}
+}
---
+++ Jicker/org/jicker/util/TestOpenBrowser.java
@@ -0,0 +1,16 @@
+package org.jicker.util;
+
+import java.io.File;
+
+public class TestOpenBrowser {
+
+	/**
+	 * @param <OpenBrowse>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		OpenBrowser t = new OpenBrowser(new File("C:\\thread.jspa.htm"));
+
+	}
+
+}
