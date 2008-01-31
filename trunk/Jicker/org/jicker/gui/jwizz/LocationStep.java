b8647667ca3173d98527c1acd195c983f16bce6e
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2008-01-31 15:13:07 +0000

    wizard bearbeitet, javaprog.net

--- trunk/Jicker/org/jicker/gui/jwizz/LocationStep.java
+++ trunk/Jicker/org/jicker/gui/jwizz/LocationStep.java
@@ -2,18 +2,23 @@ package org.jicker.gui.jwizz;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Method;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+
+import org.jicker.JickerGlobals;
 
 import net.javaprog.ui.wizard.AbstractStep;
 import net.javaprog.ui.wizard.DataModel;
@@ -25,17 +30,42 @@ class LocationStep extends AbstractStep {
     protected JFileChooser fc = new JFileChooser();
     
     public LocationStep(DataModel data) {
-        super("Choose location", "Please choose the installation location");
+        super("Arbeitsverzeichnis", "Verzeichnis zum speichern der Programmeigenschaften auswählen");
         this.data = data;
     }
     
     protected JComponent createComponent() {
-        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    	
+        final JRadioButton defaultLocationButton = new JRadioButton(
+                "Standardverzeichnis " + JickerGlobals.PROP_DIR, true);
+        final JRadioButton noDefaultButton = new JRadioButton(
+                "Verzeichnis ändern ...");
+        //Übergeordnete Box
         final JPanel stepComponent = new JPanel();
-        stepComponent.setLayout(new BoxLayout(stepComponent, BoxLayout.Y_AXIS));
-        stepComponent.add(Box.createVerticalGlue());
-        JPanel inputPanel = new JPanel(new BorderLayout(5, 0));
-        inputPanel.add(new JLabel("Directory:"), BorderLayout.WEST);
+        stepComponent.setLayout(new BoxLayout(stepComponent, BoxLayout.PAGE_AXIS));
+        
+        //Eingefügte Box mit 2 Radiobutton, vertikal
+        //zweier Radiobutton gruppieren
+        ButtonGroup group = new ButtonGroup();
+        group.add(defaultLocationButton);
+        group.add(noDefaultButton);
+
+        final JPanel radioButtonComponent = new JPanel();
+        
+        radioButtonComponent.setLayout(new BoxLayout(radioButtonComponent, BoxLayout.Y_AXIS));
+        radioButtonComponent.setAlignmentY(0.0f);
+        radioButtonComponent.add(defaultLocationButton);
+        radioButtonComponent.add(noDefaultButton);
+        
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        
+        //radioButtonComponent.add(Box.createVerticalGlue());
+        //stepComponent.add(Box.createVerticalGlue());
+        //JPanel inputPanel = new JPanel(new BorderLayout(5, 0));
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel,BoxLayout.X_AXIS));
+        //inputPanel.add(new JLabel("Directory:"), BorderLayout.WEST);
+        inputPanel.add(new JLabel("Directory:"));
         inputPanel.add(fileTextField);
         JButton browseButton = new JButton("Browse...");
         browseButton.addActionListener(new ActionListener() {
@@ -45,9 +75,10 @@ class LocationStep extends AbstractStep {
                 }
             }
         });
-        inputPanel.add(browseButton, BorderLayout.EAST);
-        inputPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 
-                inputPanel.getPreferredSize().height));
+        //inputPanel.add(browseButton, BorderLayout.EAST);
+        inputPanel.add(browseButton);
+        inputPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, inputPanel.getPreferredSize().height));
+        stepComponent.add(radioButtonComponent);
         stepComponent.add(inputPanel);
         stepComponent.add(Box.createVerticalGlue());
         Method method = null;
