75e5dc2f31ef82963efe0138a251c664b92e5cbf
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2008-02-05 14:03:02 +0000

    JUnit integriert

---
+++ trunk/Jicker/src/org/jicker/test/Test.java
@@ -0,0 +1,14 @@
+package org.jicker.test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestSrc ts = new TestSrc("test");
+		ts.myMethod();
+		System.out.println(ts.myMethod());
+	}
+
+}
