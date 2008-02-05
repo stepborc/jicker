1ad8fc71c59657e3db5c9e3bceb329ebae1d83be
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2008-02-05 14:03:02 +0000

    JUnit integriert

--- Jicker/src/org/jicker/test/TestSrc.java
+++ Jicker/src/org/jicker/test/TestSrc.java
@@ -1,5 +1,13 @@
 package org.jicker.test;
 
 public class TestSrc {
+String test;
+	public TestSrc(String test){
+		this.test = test.toUpperCase();
+		System.out.println( test + "\t" + this.test );
+	}
+	public String myMethod(){
+		return this.test;
+	}
 
 }
---
+++ Jicker/src/test/TestTestSrc.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.jicker.test.TestSrc;
+import org.junit.Test;
+
+public class TestTestSrc {
+
+	@Test
+	public final void testMyMethod() {
+		TestSrc ts = new TestSrc("test");
+		assertTrue("TEST".equals(ts.myMethod()));
+	}
+
+}
