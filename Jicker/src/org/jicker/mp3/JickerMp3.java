d3e8e88b7d0a0f9d07bdc47e1d9dee62a7917b8a
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2009-01-07 15:05:01 +0000

    Neues Package org.jicker.mp3; Commons-Io verschoben

---
+++ Jicker/src/org/jicker/mp3/JickerMp3.java
@@ -0,0 +1,17 @@
+package org.jicker.mp3;
+
+import org.jicker.mp3.data.GetData;
+
+public class JickerMp3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Datenabfrage starten
+		GetData gd = new GetData();
+		// GUI aufbauen
+
+	}
+
+}
