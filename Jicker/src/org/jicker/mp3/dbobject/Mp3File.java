770b91b9bcfebc735b182b7125627439dc4296ed
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2009-07-20 13:19:11 +0000

    Datenbankobject erweitert, Tabelle erweitert

--- Jicker/src/org/jicker/mp3/dbobject/Mp3File.java
+++ Jicker/src/org/jicker/mp3/dbobject/Mp3File.java
@@ -1,44 +1,79 @@
 package org.jicker.mp3.dbobject;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 public class Mp3File {
 	File mp3Dir;
 	File mp3File;
 	File mp3Complett;
-	//Konstruktor wenn alle Parametern des Objekts bekannt sind
+	long mp3CRC32;
+
+	// Konstruktor wenn alle Parametern des Objekts bekannt sind
 	public Mp3File(File mp3Dir, File mp3File, File mp3Complett) {
-		this.mp3Dir = mp3Dir;
-		this.mp3File = mp3File;
-		this.mp3Complett = mp3Complett;
+		this.setMp3Dir(mp3Dir);
+		this.setMp3File(mp3File);
+		this.setMp3Complett(mp3Complett);
+		this.setMp3CRC32(mp3Complett);
 	}
-	//Konstruktor wenn nur kompletter Dateiname bekannt ist
-	public Mp3File(File Mp3Complett){
-		this.mp3Complett = mp3Complett;
-		this.mp3File = new File(mp3Complett.toString().substring(mp3Complett.toString().lastIndexOf("\\")+1, mp3Complett.toString().length()));
-		this.mp3Dir = new File(mp3Complett.toString().substring(0, mp3Complett.toString().lastIndexOf("\\"))); 
+
+	// Konstruktor wenn nur kompletter Dateiname bekannt ist
+	public Mp3File(File Mp3Complett) {
+		this.setMp3Complett(mp3Complett);
+		this.mp3File = new File(mp3Complett.toString().substring(
+				mp3Complett.toString().lastIndexOf("\\") + 1,
+				mp3Complett.toString().length()));
+		this.mp3Dir = new File(mp3Complett.toString().substring(0,
+				mp3Complett.toString().lastIndexOf("\\")));
+		this.setMp3CRC32(mp3Complett);
 	}
+
 	public Mp3File(Object mp3Complett) {
 		this.mp3Complett = new File(mp3Complett.toString());
-		this.mp3File = new File(mp3Complett.toString().substring(mp3Complett.toString().lastIndexOf("\\")+1, mp3Complett.toString().length()));
-		this.mp3Dir = new File(mp3Complett.toString().substring(0, mp3Complett.toString().lastIndexOf("\\"))); 
+		this.mp3File = new File(mp3Complett.toString().substring(
+				mp3Complett.toString().lastIndexOf("\\") + 1,
+				mp3Complett.toString().length()));
+		this.mp3Dir = new File(mp3Complett.toString().substring(0,
+				mp3Complett.toString().lastIndexOf("\\")));
+		this.setMp3CRC32((File) mp3Complett);
 	}
+
 	public File getMp3Dir() {
 		return mp3Dir;
 	}
+
 	public void setMp3Dir(File mp3Dir) {
 		this.mp3Dir = mp3Dir;
 	}
+
 	public File getMp3File() {
 		return mp3File;
+	}
+
+	public long getMp3CRC32(){
+		return mp3CRC32;
 	}
 	public void setMp3File(File mp3File) {
 		this.mp3File = mp3File;
 	}
+
 	public File getMp3Complett() {
 		return mp3Complett;
 	}
+
 	public void setMp3Complett(File mp3Complett) {
 		this.mp3Complett = mp3Complett;
+	}
+
+	public void setMp3CRC32(File mp3Complett) {
+		try {
+			this.mp3CRC32 = FileUtils.checksumCRC32(mp3Complett);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			this.mp3CRC32 = 0;
+		}
 	}
 }
--- Jicker/src/org/jicker/mp3/ui/MainFrame.java
+++ Jicker/src/org/jicker/mp3/ui/MainFrame.java
@@ -1,11 +1,10 @@
 package org.jicker.mp3.ui;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Vector;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -14,6 +13,13 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
+
+import org.apache.commons.io.FileUtils;
+import org.jicker.mp3.dbobject.Mp3File;
+import org.neodatis.odb.ODB;
+import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.Objects;
+import org.neodatis.odb.impl.core.query.values.ValuesCriteriaQuery;
 
 public class MainFrame extends JFrame implements ActionListener {
 	private static final long serialVersionUID = -3316519290894600666L;
@@ -28,8 +34,28 @@ public class MainFrame extends JFrame implements ActionListener {
 				(d.height - getSize().height) / 2);
 
 		setVisible(true);
+		ODB odb = ODBFactory.open("jickerMp3.odb");
+		System.out.println(odb.getValues(new ValuesCriteriaQuery(Mp3File.class)
+				.count("mp3Complett")));
 
-		table.getModel().setValueAt("test1", 0, 0);
+		Objects<Mp3File> mp3Files = odb.getObjects(Mp3File.class);
+		Mp3File mp3File = null;
+		int n = 0;
+		while (mp3Files.hasNext()) {
+			mp3File = mp3Files.next();
+			table.getModel().setValueAt(mp3File.getMp3Complett(), n, 0);
+			table.getModel().setValueAt(mp3File.getMp3CRC32(), n, 1);
+			try {
+				table.getModel().setValueAt(FileUtils.checksumCRC32(mp3File.getMp3Complett()), n, 2);
+			} catch (IOException e) {
+				table.getModel().setValueAt("0", n, 2);
+				//e.printStackTrace();
+			}
+			n = n + 1;
+		}
+		odb.close();
+
+		/*
 		table.getModel().setValueAt("", 0, 1);
 		table.getModel().setValueAt("test2", 1, 0);
 		table.getModel().setValueAt("", 1, 1);
@@ -41,6 +67,7 @@ public class MainFrame extends JFrame implements ActionListener {
 		table.getModel().setValueAt("", 4, 1);
 		table.getModel().setValueAt("test6", 5, 0);
 		table.getModel().setValueAt("", 5, 1);
+		*/
 		JScrollPane pane = new JScrollPane(table);
 
 		JPanel panel = new JPanel();
@@ -56,15 +83,16 @@ public class MainFrame extends JFrame implements ActionListener {
 		panel.updateUI();
 	}
 
-	public static void main(String arguments[]) {
-		MainFrame a = new MainFrame();
+	public static void main(String arguments[]) throws IOException {
+		//MainFrame a = new MainFrame();
+		new MainFrame();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		int rowcount = ((AbstractTableModel) table.getModel()).getRowCount();
-		int colcount = ((AbstractTableModel) table.getModel()).getColumnCount();
+		//int colcount = ((AbstractTableModel) table.getModel()).getColumnCount();
 
 		System.out.println("___________________________________________");
 		for (int i = 0; i < rowcount; i++) {
