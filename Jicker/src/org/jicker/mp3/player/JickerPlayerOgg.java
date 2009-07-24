7bb7790d440da2424151717348eace9df6a68e63
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2009-07-24 11:56:03 +0000

    Codebereinigungen

--- Jicker/src/org/jicker/mp3/player/JickerPlayerOgg.java
+++ Jicker/src/org/jicker/mp3/player/JickerPlayerOgg.java
@@ -19,72 +19,62 @@ public class JickerPlayerOgg {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws UnsupportedAudioFileException 
-	 * @throws LineUnavailableException 
+	 * @throws IOException
+	 * @throws UnsupportedAudioFileException
+	 * @throws LineUnavailableException
 	 */
-	public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-		//PrintStream out = System.out;
-		File file = new File("C:/Java/android-sdk-windows-1.0_r1/tools/lib/res/default/raw/fallbackring.ogg");
+	public static void main(String[] args) throws IOException,
+			UnsupportedAudioFileException, LineUnavailableException {
+		String filename = "C:/Java/android-sdk-windows-1.0_r1/tools/lib/res/default/raw/fallbackring.ogg";
+		File file = new File(filename);
 		AudioFileFormat aff = AudioSystem.getAudioFileFormat(file);
-		//if (out != null) out.println("Audio Type : "+aff.getType());
-		AudioInputStream in= AudioSystem.getAudioInputStream(file);
+		System.out.println("Audio Type : " + aff.getType());
+		AudioInputStream in = AudioSystem.getAudioInputStream(file);
 		AudioInputStream din = null;
-		if (in != null)
-		{
-		  AudioFormat baseFormat = in.getFormat();
-		  //if (out != null) out.println("Source Format : "+baseFormat.toString());
-		  AudioFormat  decodedFormat = new AudioFormat(
-			  AudioFormat.Encoding.PCM_SIGNED,
-			  baseFormat.getSampleRate(),
-			  16,
-			  baseFormat.getChannels(),
-			  baseFormat.getChannels() * 2,
-			  baseFormat.getSampleRate(),
-			  false);
-		  //if (out != null) out.println("Target Format : "+decodedFormat.toString());
-		  din = AudioSystem.getAudioInputStream(decodedFormat, in);
-		  if (din instanceof PropertiesContainer)
-		  {
-			//assertTrue("PropertiesContainer : OK",true);
-		  }
-		  else
-		  {
-			//assertTrue("Wrong PropertiesContainer instance",false);
-		  }
-		  rawplay(decodedFormat, din);
-		  in.close();		
-		  //if (out != null) out.println("---  Stop : "+filename+"  ---");
+		if (in != null) {
+			AudioFormat baseFormat = in.getFormat();
+			System.out.println("Dateiformat : " + baseFormat.toString());
+			AudioFormat decodedFormat = new AudioFormat(
+					AudioFormat.Encoding.PCM_SIGNED,
+					baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+					baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+					false);
+			System.out.println("Ausgabeformat : " + decodedFormat.toString());
+			din = AudioSystem.getAudioInputStream(decodedFormat, in);
+			rawplay(decodedFormat, din);
+			in.close();
+			System.out.println("---  Stop : " + filename + "  ---");
 		}
-	 }
-	
-	private static SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException
-	{
-	  SourceDataLine res = null;
-	  DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-	  res = (SourceDataLine) AudioSystem.getLine(info);
-	  res.open(audioFormat);
-	  return res;
 	}
-	private static void rawplay(AudioFormat targetFormat, AudioInputStream din) throws IOException, LineUnavailableException
-	{
+
+	private static SourceDataLine getLine(AudioFormat audioFormat)
+			throws LineUnavailableException {
+		SourceDataLine res = null;
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class,
+				audioFormat);
+		res = (SourceDataLine) AudioSystem.getLine(info);
+		res.open(audioFormat);
+		return res;
+	}
+
+	private static void rawplay(AudioFormat targetFormat, AudioInputStream din)
+			throws IOException, LineUnavailableException {
 		byte[] data = new byte[4096];
-		SourceDataLine line = getLine(targetFormat);		
-		if (line != null)
-		{
-		  // Start
-		  line.start();
-		  int nBytesRead = 0, nBytesWritten = 0;
-		  while (nBytesRead != -1)
-		  {
-			nBytesRead = din.read(data, 0, data.length);
-			if (nBytesRead != -1) nBytesWritten = line.write(data, 0, nBytesRead);
-		  }
-		  // Stop
-		  line.drain();
-		  line.stop();
-		  line.close();
-		  din.close();
-		}		
+		SourceDataLine line = getLine(targetFormat);
+		if (line != null) {
+			// Start
+			line.start();
+			int nBytesRead = 0, nBytesWritten = 0;
+			while (nBytesRead != -1) {
+				nBytesRead = din.read(data, 0, data.length);
+				if (nBytesRead != -1)
+					nBytesWritten = line.write(data, 0, nBytesRead);
+			}
+			// Stop
+			line.drain();
+			line.stop();
+			line.close();
+			din.close();
+		}
 	}
 }
