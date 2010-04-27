7f64c18f70ad82a69b7f9631bc80c03edc2e1193
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2010-04-27 06:44:21 +0000

    ValueOfQuotient angepasst

--- Jicker/src/org/jicker/gc1r3r6/ValueOfQuotient.java
+++ Jicker/src/org/jicker/gc1r3r6/ValueOfQuotient.java
@@ -1,16 +1,38 @@
+/*
+ *
+ */
 package org.jicker.gc1r3r6;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.concurrent.ArrayBlockingQueue;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Divisor.
+ */
 
 public class ValueOfQuotient {
-
 	/**
-	 * @param args
+	 * The main method.
+	 *
+	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
+		ArrayList<Long> d = new ValueOfQuotient().voq();
+		for (int index = 0; index < d.size(); index++)
+			System.out.println(d.get(index));
+		System.out.println(d.size());
+
+	}
+
+	/**
+	 * Der Divisor kann die Form yxwvr3 annehmen
+	 * Erstelle eine ArrayList<Long> mit allen möglichen Werten,
+	 * die an der 3 Position von links eine 3 haben und .
+	 *
+	 * @return the array list
+	 */
+	public ArrayList<Long> voq() {
 		long right;
 		ArrayList<Long> voq = new ArrayList<Long>();
 		for (int r = 0; r <= 9; r++) {
@@ -21,7 +43,7 @@ public class ValueOfQuotient {
 							right = (y * 100000) + (x * 10000) + (w * 1000)
 									+ (v * 100) + (r * 10) + 3;
 							Long lRight = null;
-							for (int n = 1; n <= 9; n++) {
+							for (int n = 2; n <= 9; n++) {
 								lRight = Long.valueOf(right * n);
 								if (lRight.toString().length() == 7) {
 									if (lRight.toString().substring(2, 3)
@@ -36,7 +58,6 @@ public class ValueOfQuotient {
 			}
 		}
 		Collections.sort(voq);
-		for (int index = 0; index < voq.size(); index++)
-			System.out.println(voq.get(index));
+		return voq;
 	}
 }
