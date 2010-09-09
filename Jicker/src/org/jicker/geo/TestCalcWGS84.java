fbf6d4bcf0912b07ffb3fd377e0bbf95e7e77a13
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2010-09-09 13:24:59 +0000

    Refactoring

--- Jicker/src/org/jicker/geo/TestCalcWGS84.java
+++ Jicker/src/org/jicker/geo/TestCalcWGS84.java
@@ -6,8 +6,18 @@ public class TestCalcWGS84 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new CalcWGS84().main(null);
 
+        System.out.println("Gegeben");
+        int sBreiteGrad = 51;
+        double sBreiteMinute = 23.260;
+        int sLaengeGrad = 6;
+        double sLaengeMinute = 48.372;
+        double sRichtung = 90.0;
+        double sEntfernung = 0.180;
+        //N51°23.339 E006°48.564
+        System.out.println("Berechnet");
+		CalcWGS84 c = new CalcWGS84(sBreiteGrad, sBreiteMinute, sLaengeGrad, sLaengeMinute, sRichtung, sEntfernung);
+		System.out.println(c.getBreite());
 	}
 
 }
