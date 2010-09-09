e847a3ac54f7e42b5ee1120a0f8a14e32ffec4cd
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2010-09-09 13:24:59 +0000

    Refactoring

--- trunk/Jicker/src/org/jicker/geo/CalcWGS84.java
+++ trunk/Jicker/src/org/jicker/geo/CalcWGS84.java
@@ -2,64 +2,70 @@ package org.jicker.geo;
 
 public class CalcWGS84 {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("Start");
-        int sBreiteGrad = 51;
-        double sBreiteMinute = 23.260;
-        int sLaengeGrad = 6;
-        double sLaengeMinute = 48.372;
-        int sRichtung = 90;
-        double sEntfernung = 0.180;
-        //N51°23.339 E006°48.564
-        calcNewCoords(sBreiteGrad, sBreiteMinute, sLaengeGrad, sLaengeMinute, sRichtung, sEntfernung);
+	private int nLaengeMinute;
+	private String nLaengeGrad;
+	public CalcWGS84(int sBreiteGrad, double sBreiteMinute, int sLaengeGrad,
+			double sLaengeMinute, double sRichtung, double sEntfernung) {
+		calcNewCoords(sBreiteGrad, sBreiteMinute, sLaengeGrad, sLaengeMinute,
+				sRichtung, sEntfernung);
+	}
 
-    }
-private static double decBreite(int sBreiteGrad, double sBreiteMinute){
-    return Math.PI/180*(sBreiteGrad+(sBreiteMinute/60));
-}
-private static double decLaenge(int sLaengeGrad, double sLaengeMinute){
-    return Math.PI/180*(sLaengeGrad+(sLaengeMinute/60));
+	private static double decBreite(int sBreiteGrad, double sBreiteMinute) {
+		return Math.PI / 180 * (sBreiteGrad + (sBreiteMinute / 60));
+	}
 
-}
-    private static void calcNewCoords(int sBreiteGrad, double sBreiteMinute, int sLaengeGrad, double sLaengeMinute, int sRichtung, double sEntfernung) {
-        double tmpBreite =  decBreite(sBreiteGrad, sBreiteMinute);
-        double tmpLaenge = decLaenge(sLaengeGrad, sLaengeMinute);
-        double tmpRichtung = Math.PI/180*sRichtung;
-        double tmpEntfernung = (Math.PI/(180*60))*sEntfernung/1.852;
-        System.out.println("Breite: " + tmpBreite);
-        System.out.println("Länge: " + tmpLaenge);
-        System.out.println("Richtung: " + tmpRichtung);
-        System.out.println("Entfernung: " + tmpEntfernung);
+	private static double decLaenge(int sLaengeGrad, double sLaengeMinute) {
+		return Math.PI / 180 * (sLaengeGrad + (sLaengeMinute / 60));
+	}
 
-        double lat = Math.asin(Math.sin(tmpBreite)*Math.cos(tmpEntfernung)+Math.cos(tmpBreite)*Math.sin(tmpEntfernung)*Math.cos(tmpRichtung));
-        // Gegenüber Excel sind x und y in der Methode atan2 vertauscht
-        Double dlon = (Math.atan2(Math.sin(tmpRichtung)*Math.sin(tmpEntfernung)*Math.cos(tmpBreite),Math.cos(tmpEntfernung)-Math.sin(tmpBreite)*Math.sin(lat)))*-1;
-        Double pLon = tmpLaenge - dlon + Math.PI;
-        Double pLonn = pLon/2/Math.PI;
+	private static double decRichtung(Double lon) {
+		return Math.PI / 180 * lon;
+	}
 
-        Double lon = pLon - pLonn.intValue() - Math.PI;
-        System.out.println("Latidude: " + lat);
-        System.out.println("dlon: " + dlon);
-        System.out.println("lon: " + lon);
+	private static double decEntfernung(double sEntfernung) {
+		return (Math.PI / (180 * 60)) * sEntfernung / 1.852;
+	}
 
-        Double tmpNeueBreiteGrad = 180/Math.PI*lat;
-        int nBreiteGrad = tmpNeueBreiteGrad.intValue();
-        Double nBreiteMinute =(tmpNeueBreiteGrad - nBreiteGrad) * 60;
-        System.out.println("Neue Breite: " + nBreiteGrad + "° " + nBreiteMinute);
+	private static void calcNewCoords(int sBreiteGrad, double sBreiteMinute,
+			int sLaengeGrad, double sLaengeMinute, double sRichtung,
+			double sEntfernung) {
 
-        Double tmpNeueLaengeGrad = 180/Math.PI*lon;
-        int nLaengeGrad = tmpNeueLaengeGrad.intValue();
-        Double nLaengeMinute =(tmpNeueLaengeGrad - nLaengeGrad) * 60;
-        System.out.println("Neue Länge: " + nLaengeGrad + "° " + nLaengeMinute);
+		double lat = Math.asin(Math.sin(decBreite(sBreiteGrad, sBreiteMinute))
+				* Math.cos(decEntfernung(sEntfernung))
+				+ Math.cos(decBreite(sBreiteGrad, sBreiteMinute))
+				* Math.sin(decEntfernung(sEntfernung))
+				* Math.cos(decRichtung(sRichtung)));
+		// Gegenüber Excel sind x und y in der Methode atan2 vertauscht
+		Double dlon = Math.atan2(
+				Math.sin(decRichtung(sRichtung))
+						* Math.sin(decEntfernung(sEntfernung))
+						* Math.cos(decBreite(sBreiteGrad, sBreiteMinute)),
+				Math.cos(decEntfernung(sEntfernung))
+						- Math.sin(decBreite(sBreiteGrad, sBreiteMinute))
+						* Math.sin(lat))
+				* -1;
+		Double pLon = decLaenge(sLaengeGrad, sLaengeMinute) - dlon + Math.PI;
+		Double pLonn = pLon / 2 / Math.PI;
 
+		Double lon = pLon - pLonn.intValue() - Math.PI;
+		System.out.println("Latidude: " + lat);
+		System.out.println("dlon: " + dlon);
+		System.out.println("lon: " + lon);
 
+		Double tmpNeueBreiteGrad = 180 / Math.PI * lat;
+		int nBreiteGrad = tmpNeueBreiteGrad.intValue();
+		Double nBreiteMinute = (tmpNeueBreiteGrad - nBreiteGrad) * 60;
+		System.out
+				.println("Neue Breite: " + nBreiteGrad + "° " + nBreiteMinute);
 
-
-
-
-    }
-
+		Double tmpNeueLaengeGrad = 180 / Math.PI * lon;
+		//Double tmpNeueLaengeGrad = decRichtung(lon);
+		int nLaengeGrad = tmpNeueLaengeGrad.intValue();
+		Double nLaengeMinute = (tmpNeueLaengeGrad - nLaengeGrad) * 60;
+		System.out.println("Neue Länge: " + nLaengeGrad + "° " + nLaengeMinute);
+	}
+	public String getBreite(){
+		return nLaengeGrad + "° " + nLaengeMinute;
+		
+	}
 }
