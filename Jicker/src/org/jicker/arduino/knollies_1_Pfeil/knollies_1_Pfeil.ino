8fffa08e2f8c2ce5cbffe59d7ee69e32743e7504
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2012-01-06 07:56:25 +0000

    correct package name to org instead of orj modified pfeil sketch

--- Jicker/src/org/jicker/arduino/knollies_1_Pfeil/knollies_1_Pfeil.ino
+++ Jicker/src/org/jicker/arduino/knollies_1_Pfeil/knollies_1_Pfeil.ino
@@ -10,8 +10,13 @@ int clockPin = 12;
 int dataPin = 11;
 
 uint8_t led[8];
+uint8_t ledUp[8];
+uint8_t ledDown[8];
+
 long counter1 = 0;
 long counter2 = 0;
+
+arrowDirection = "down";
 
 void setup() {
   //set pins to output 
@@ -19,44 +24,65 @@ void setup() {
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   //digitalWrite(latchPin, HIGH);
-  led[0] = B00001000;
-  led[1] = B00011100;
-  led[2] = B00111110;
-  led[3] = B00011100;
-  led[4] = B00011100;
-  led[5] = B00011100;
-  led[6] = B00011100;
-  led[7] = B00011100;
+  ledUp[0] = B00001000;
+  ledUp[1] = B00011100;
+  ledUp[2] = B00111110;
+  ledUp[3] = B00011100;
+  ledUp[4] = B00011100;
+  ledUp[5] = B00011100;
+  ledUp[6] = B00011100;
+  ledUp[7] = B00011100;
+  ledDown[0] = B00001000;
+  ledDown[1] = B00011100;
+  ledDown[2] = B00111110;
+  ledDown[3] = B00011100;
+  ledDown[4] = B00011100;
+  ledDown[5] = B00011100;
+  ledDown[6] = B00011100;
+  ledDown[7] = B00011100;
   Timer1.initialize(10000);
   Timer1.attachInterrupt(screenUpdate);
 }
 
 void loop() {
   counter1++;
-    if (counter1 >=100000) {counter2++;}
-if (counter2 >= 10000) {
-  counter1 = 0;
-  counter2 = 0;
-  for (int i=0; i<8; i++) {
-    //led[i]= ~led[i];
-    led[i]=led[7-i];
+  if (counter1 >=100000) {
+    counter2++;
   }
+  if (counter2 >= 10000) {
+    counter1 = 0;
+    counter2 = 0;
+
+    //for (int i=0; i<8; i++) {
+    //led[i]= ~led[i];
+  }
+
+  if (arrowDirection == "up"){
+    for (int i=0;i<8;i++){
+          led[i]= ledDown[i];
+    }
+    arrowDirection = "down";
+  }
+  else if (arrowDirection == "down"){
+    for (int i=0;i<8;i++){
+          led[i]= ledUp[i];
+    }
+    arrowDirection = "up";
   }
 }
 
 
-
 void screenUpdate() {
-uint8_t row = B00000001;
- for (byte k = 0; k < 9; k++) {
+  uint8_t row = B00000001;
+  for (byte k = 0; k < 9; k++) {
     digitalWrite(latchPin, LOW); // Open up the latch ready to receive data
 
-      shiftIt(~row ); 
-      shiftIt(led[k] ); // LED array
+    shiftIt(~row ); 
+    shiftIt(led[k] ); // LED array
 
-   digitalWrite(latchPin, HIGH); // Close the latch, sending the data in the registers out to the matrix
+    digitalWrite(latchPin, HIGH); // Close the latch, sending the data in the registers out to the matrix
     row = row << 1;
- }
+  }
 
 }
 
@@ -72,7 +98,7 @@ void shiftIt(byte dataOut) {
 
   // for each bit in dataOut send out a bit
   for (int i=7; i>=0; i--)  {
-      //set clockPin to LOW prior to sending bit
+    //set clockPin to LOW prior to sending bit
     digitalWrite(clockPin, LOW);
 
     // if the value of DataOut and (logical AND) a bitmask
@@ -94,5 +120,6 @@ void shiftIt(byte dataOut) {
   //stop shifting
   digitalWrite(clockPin, LOW);
 }
+
 
 
