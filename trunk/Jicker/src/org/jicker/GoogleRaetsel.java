22618ccf598518a13c202a4f5034535ded588ccf
stepborc <stepborc@e63418c4-c234-0410-a210-dd3f49822de1>
2012-01-06 07:56:25 +0000

    correct package name to org instead of orj modified pfeil sketch

---
+++ trunk/Jicker/src/org/jicker/GoogleRaetsel.java
@@ -0,0 +1,23 @@
+package org.jicker;
+
+public class GoogleRaetsel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Integer p1 = 2;
+		Integer p2 = 10;
+		Integer p3 = 11;
+		Integer p4 = 12;
+		String op1 = "";
+		String op2 = "" ;;
+		String op3 = "";
+		
+		for(int n=0;n<=5;n++){
+			if (){
+		}
+
+	}
+
+}
